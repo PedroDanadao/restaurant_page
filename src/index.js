c9446fc7d6c4cccc050f1aaef0e5e4fb5938bd2a
PedroDanadao <15200544+PedroDanadao@users.noreply.github.com>
2025-09-04 14:19:19 -0300

    First commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+console.log("Ok");
+
+
+
+/**
+ <img src="./images/logo_icon.png" alt="Logo image of burger">
+ <span class="logo_name">Denny's Burgers</span>
+ 
+ <h1 class="headline">Denny's Burgers: Crafted for Cravings.</h1>
+ <p>- Unforgettable flavor in every bite.</p>
+ * 
+ */
+
